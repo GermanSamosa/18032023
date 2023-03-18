058e64255a06d82fa2ea797fae66e49723fcccc5
Matthew Rao <mat_rao@hotmail.com>
2023-03-18 11:43:24 -0400

    added typescript
    thank god lol

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <h1>Wasguuuuud</h1>
       </header>
     </div>
+    </>
   );
 }
 
